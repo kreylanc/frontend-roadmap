36d4c9c8a9373863fbca549811610be02175dd18
Kreylanc <devjanpun@gmail.com>
2023-05-22 13:56:46 +0100

    rich text package

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,14 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        purple: "#100014",
+      },
+      fontFamily: {
+        sans: ["var(--font-raleway)"],
+      },
+    },
   },
   plugins: [],
 };
