79afa60b64c9a77642639713e8cf4c9428cc8ed6
Kreylanc <devjanpun@gmail.com>
2023-06-06 13:48:30 +0100

    UI updates

--- components/BlogList.tsx
+++ components/BlogList.tsx
@@ -8,17 +8,14 @@ type Props = {
 //
 function BlogList({ posts }: Props) {
   return (
-    <div className=" w-full lg:w-9/12 mx-auto mt-8" id="content">
+    <ol className=" w-full lg:w-9/12 mx-auto mt-8" id="content">
       {/* Home page all posts display */}
       {posts.map((el, i) => (
-        <section className="p-4 md:p-8" key={el._id}>
-          <h2
-            data-before={i + 1}
-            className={`text-2xl md:text-3xl font-bold before:content-[attr(data-before)] before:text-primaryYellow before:bg-zinc-700 before:rounded-[50%] before:px-3 before:py-1 before:mr-4`}
-          >
+        <li className="p-4 md:p-8 list-decimal" key={el._id}>
+          <h2 data-before={i + 1} className={`text-4xl  font-bold  `}>
             {el.title}
           </h2>
-          <div className="group grid grid-cols-fluid gap-y-6 gap-x-12 mt-8 ml-8">
+          <section className="group grid grid-cols-fluid gap-y-6 gap-x-12 mt-8">
             {el.post.map((item) => (
               <ClientSideRoute
                 key={item._id}
@@ -31,10 +28,10 @@ function BlogList({ posts }: Props) {
                 />
               </ClientSideRoute>
             ))}
-          </div>
-        </section>
+          </section>
+        </li>
       ))}
-    </div>
+    </ol>
   );
 }
 
--- components/Card.jsx
+++ components/Card.jsx
@@ -3,16 +3,20 @@ import urlFor from "../lib/urlFor";
 
 function Card({ title, description, imgUrl }) {
   return (
-    <div className="relative bg-neutral-700 h-full p-4 hover:bg-primaryYellow hover:text-zinc-900 group transition-colors">
-      <h3 className="text-lg md:text-2xl font-semibold">{title}</h3>
-      <p className="mt-2 text-base md:text-lg">{description}</p>
-      {/* <Image
-        className="absolute bottom-8 right-8"
-        src={urlFor(imgUrl).url()}
-        alt={title}
-        width={60}
-        height={60}
-      /> */}
+    <div className="relative flex flex-col bg-zinc-700 h-full p-4 md:p-6 border-2 border-zinc-700 hover:border-primaryYellow hover:text-zinc-200 group transition-colors">
+      <h3 className="text-3xl font-semibold">{title}</h3>
+      {imgUrl ? (
+        <Image
+          className=" w-auto h-24 md:h-28 object-contain py-4 self-start"
+          src={urlFor(imgUrl).url()}
+          alt={imgUrl.alt}
+          width={80}
+          height={80}
+        />
+      ) : (
+        <div className="h-24 py-4 text-rose-500">Cant load image</div>
+      )}
+      <p className="mt-2">{description}</p>
     </div>
   );
 }
