5e58ddfc0e876cba558e9184ee4c04450aa31468
Kreylanc <devjanpun@gmail.com>
2023-06-06 13:48:44 +0100

    update sanity.config

--- sanity.config.ts
+++ sanity.config.ts
@@ -4,8 +4,8 @@ import { deskTool } from "sanity/desk";
 import { schemaTypes } from "./schemas";
 import { codeInput } from "@sanity/code-input";
 
-const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
-const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET;
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!;
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET!;
 export default defineConfig({
   basePath: "/studio",
   name: "Roadmap_Content_Studio",
