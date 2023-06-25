a2249ee3eec6248691ea46170984541089998d22
Kreylanc <devjanpun@gmail.com>
2023-06-25 20:19:15 +0100

    Card UI Update

--- components/Card.jsx
+++ components/Card.jsx
@@ -3,19 +3,22 @@ import urlFor from "../lib/urlFor";
 
 function Card({ title, description, imgUrl }) {
   return (
-    <div className="relative flex flex-col bg-zinc-700 h-full p-4 md:p-6 border-2 border-zinc-700 hover:border-primaryYellow hover:text-zinc-200 group transition-colors">
-      <h3 className="text-3xl font-semibold">{title}</h3>
+    <div className="relative flex flex-col gap-y-2 h-full p-4 lg:p-6">
       {imgUrl ? (
         <Image
-          className=" w-auto h-24 md:h-28 object-contain py-4 self-start"
+          className=" w-auto h-16 md:h-24 lg:h-28 object-contain self-start"
           src={urlFor(imgUrl).url()}
           alt={imgUrl.alt}
           width={80}
           height={80}
         />
       ) : (
-        <div className="h-24 py-4 text-rose-500">Cant load image</div>
+        <div className="h-24 text-rose-500">Cant load image</div>
       )}
+      <h3 className="text-3xl mt-4 break-words font-semibold text-neutral-50">
+        {title}
+      </h3>
+
       <p className="mt-2">{description}</p>
     </div>
   );
