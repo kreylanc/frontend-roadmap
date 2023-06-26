a884c893112a95170780f43c7a1b583cc09bd2e8
Kreylanc <devjanpun@gmail.com>
2023-06-26 10:07:53 +0100

    Add icon and footer

--- app/(user)/layout.jsx
+++ app/(user)/layout.jsx
@@ -1,11 +1,11 @@
 import "../globals.css";
 import { Raleway } from "next/font/google";
 import Header from "../../components/Header";
+import Footer from "../../components/Footer";
 import { Analytics } from "@vercel/analytics/react";
 
 const raleway = Raleway({
   subsets: ["latin"],
-  weight: ["400", "500", "600", "700"],
   variable: "--font-raleway",
   display: "swap",
 });
@@ -17,19 +17,20 @@ export const metadata = {
   },
   description: "A roadmap for frontend web development",
   icons: {
-    icon: "../../public/vercel.svg",
+    icon: "/logo.png",
   },
 };
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className={raleway.variable}>
-      <body className="bg-darkPurple font-sans text-neutral-300">
-        <div className="text-center bg-red-700 text-neutral-50">
-          Website in development
+    <html lang="en" className={`${raleway.variable} font-sans`}>
+      <body className="bg-darkPurple text-neutral-300">
+        <div className="text-center  text-red-400 w-full">
+          Website in development! Not an indicative of final product.
         </div>
         <Header />
         {children}
+        <Footer />
         <Analytics />
       </body>
     </html>
---
+++ components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer className="h-48 bg-neutral-600 text-neutral-700 mt-12 px-4 md:px-8 py-6">
+      <h2>Footer</h2>
+    </footer>
+  );
+}
+
+export default Footer;
