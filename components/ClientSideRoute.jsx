d1b56442b0d7866a13c73e22f74192b2ae329995
Kreylanc <devjanpun@gmail.com>
2023-09-06 11:26:22 +0100

    UI: effect update for hover and focu

--- components/ClientSideRoute.jsx
+++ components/ClientSideRoute.jsx
@@ -6,7 +6,7 @@ function ClientSideRoute({ children, route }) {
   return (
     <Link
       href={route}
-      className="h-auto ring-zinc-700 ring-2  focus:ring-primaryYellow rounded-md focus:outline-0  hover:ring-primaryYellow transition-shadow shadow-light dark:shadow-neu"
+      className="h-auto ring-zinc-700 ring-2  focus:ring-primaryYellow rounded-md focus:outline-0  hover:ring-primaryYellow hover:bg-gray-300/20 dark:hover:bg-gray-600/40 dark:focus:bg-gray-600/40 transition-all shadow-light dark:shadow-neu"
     >
       {children}
     </Link>
