452baee79bcc3af6a8eb607f45607246d7be32a1
Kreylanc <devjanpun@gmail.com>
2023-09-24 15:53:08 +0100

    Fix: twitter image dimension

--- app/(user)/[category]/[slug]/twitter-image.tsx
+++ app/(user)/[category]/[slug]/twitter-image.tsx
@@ -9,8 +9,8 @@ export const runtime = "edge";
 // export const alt = "About Acme";
 
 export const size = {
-  width: 1200,
-  height: 800,
+  width: 512,
+  height: 512,
 };
 
 export const contentType = "image/png";
@@ -45,6 +45,7 @@ export default async function Image({ params: { slug } }: Props) {
           justifyContent: "center",
           flexDirection: "column",
           flexWrap: "nowrap",
+          padding: "8px",
         }}
       >
         <div
@@ -57,7 +58,7 @@ export default async function Image({ params: { slug } }: Props) {
         >
           <div
             style={{
-              fontSize: 82,
+              fontSize: 64,
               fontStyle: "bolder",
               letterSpacing: "-0.025em",
               lineHeight: 1.4,
@@ -68,7 +69,7 @@ export default async function Image({ params: { slug } }: Props) {
           </div>
           <div
             style={{
-              fontSize: 22,
+              fontSize: 18,
               marginTop: 18,
               lineHeight: 1.4,
               whiteSpace: "pre-wrap",
@@ -89,9 +90,9 @@ export default async function Image({ params: { slug } }: Props) {
           {/* eslint-disable-next-line @next/next/no-img-element */}
           <img
             alt="logo of stylized R using brackets and backward slash"
-            height={100}
+            height={60}
             src="https://raw.githubusercontent.com/kreylanc/frontend-roadmap/e913b635c65a87e5843abe2d395cdf109ed18c27/public/logo.svg"
-            width={100}
+            width={60}
           />
         </div>
       </div>
