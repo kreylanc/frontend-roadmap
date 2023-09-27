348b4eab71e5e7963797ebd6cdfdbb2c5370e8e6
Kreylanc <devjanpun@gmail.com>
2023-09-27 22:23:50 +0100

    Update: About page

--- app/(user)/about/page.jsx
+++ app/(user)/about/page.jsx
@@ -1,3 +1,6 @@
+import Link from "next/link";
+import { BsGithub, BsFacebook } from "react-icons/bs";
+
 export const metadata = {
   title: "About",
   description: "A roadmap for frontend web development",
@@ -12,17 +15,37 @@ export default function page() {
       <h2 className="text-3xl font-semibold">What is front-end roadmap?</h2>
       <p className="mt-4 text-lg">
         Front-end roadmap is a project created to help beginners wanting to
-        learn about web development. We provide information on different
-        technologies involved in web development process along with how each of
-        them are used. The information are categorized depending on the
-        technologies to learn.
+        learn about web development. The goal is to provide information on
+        different technologies involved in web development process along with
+        how each of them are used. The information are categorized depending on
+        the technologies to learn.
       </p>
       <p className="mt-4 text-lg">
         We provide an overview for the technology and reasons if you should use
         it or not along with code examples and interactive code sandbox where
         required.
       </p>
-      <h2 className="text-3xl font-semibold">Contact Us</h2>
+      <h2 className="text-3xl font-semibold mt-6">About Me</h2>
+      <p className="mt-4 text-lg">
+        I am a student in University of Greenwich studying MA Web Design and
+        Content Planning. I love coding and I have come to love web development
+        even more recently. This project was developed to tackle the struggles I
+        had when I started learning front-end development and maybe help people
+        like you too (if you are a beginner).
+      </p>
+      <div className="flex gap-4 mt-4">
+        <p>Connect with me on socials:</p>
+        <Link
+          href="https://github.com/kreylanc"
+          className="ml-2"
+          target="_blank"
+        >
+          <BsGithub size="1.8em" />
+        </Link>
+        <Link href="https://www.facebook.com/kreylanc" target="_blank">
+          <BsFacebook size="1.8em" />
+        </Link>
+      </div>
     </main>
   );
 }
